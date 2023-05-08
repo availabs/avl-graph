3db65bd594578c4f7bc23a4e29628a4e65caf849
Eric Conklin <eric.s.conklin@gmail.com>
2023-05-08 14:00:50 -0400

    stuff

--- src/utils/index.js
+++ src/utils/index.js
@@ -80,8 +80,6 @@ export const getScale = options => {
     domain = getter(data);
   }
 
-console.log("GET SCALE:", type)
-
   const scale = ScaleMap[type]()
     .domain(domain)
     .range(range);
