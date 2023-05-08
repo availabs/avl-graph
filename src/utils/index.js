3d0ef5632bd1d00649a1677031d46a5480759dd6
alex <am3081@gmail.com>
2023-05-08 11:50:16 -0400

    changes

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,11 +1,42 @@
 import React from "react"
+import colorbrewer from "colorbrewer"
+import isEqual from "lodash/isEqual"
+import get from "lodash/get"
 
-import deepequal from "deepequal"
-import get from "lodash.get"
+const ColorRanges = {}
 
-import { getColorRange } from "@availabs/avl-components"
+for (const type in colorbrewer.schemeGroups) {
+  colorbrewer.schemeGroups[type].forEach(name => {
+    const group = colorbrewer[name];
+    for (const length in group) {
+      if (!(length in ColorRanges)) {
+        ColorRanges[length] = [];
+      }
+      ColorRanges[length].push({
+        type: `${ type[0].toUpperCase() }${ type.slice(1) }`,
+        name,
+        category: "Colorbrewer",
+        colors: group[length]
+      })
+    }
+  })
+}
+
+export { ColorRanges };
+//console.log("ColorRanges", ColorRanges);
+
+export const getColorRange = (size, name, reverse=false) => {
+  let range = get(ColorRanges, [size], [])
+    .reduce((a, c) => c.name === name ? c.colors : a, []).slice();
+  if(reverse) {
+    range.reverse()
+  }
+  return range
+}
 
 const DEFAULT_COLORS = getColorRange(12, "Set3");
+
+
 
 export const getColorFunc = colors => {
   if (typeof colors === "function") {
@@ -54,9 +85,9 @@ export const useShouldComponentUpdate = (props, width, height) => {
 
   const ShouldComponentUpdate = React.useMemo(() => {
     const keys = get(props, "shouldComponentUpdate", []);
-    return !deepequal([width, height], prevSize.current) ||
+    return !isEqual([width, height], prevSize.current) ||
       keys.reduce((a, c) => {
-        return a || !deepequal(get(prevProps, ["current", c]), get(props, c));
+        return a || !isEqual(get(prevProps, ["current", c]), get(props, c));
       }, !Boolean(keys.length));
   }, [props, width, height]);
 
@@ -66,4 +97,32 @@ export const useShouldComponentUpdate = (props, width, height) => {
   }, [props, width, height]);
 
   return ShouldComponentUpdate;
+}
+
+export const useSetSize = (ref, callback) => {
+  const [size, setSize] = React.useState({ width: 0, height: 0, x: 0, y: 0 });
+
+  const doSetSize = React.useCallback(() => {
+    const rect = getRect(ref),
+      { width, height, x, y } = rect;
+    if ((width !== size.width) || (height !== size.height)) {
+      if (typeof callback === "function") {
+        callback({ width, height, x, y });
+      }
+      setSize({ width, height, x, y });
+    }
+  }, [ref, size, callback]);
+
+  React.useEffect(() => {
+    window.addEventListener("resize", doSetSize);
+    return () => {
+      window.removeEventListener("resize", doSetSize);
+    }
+  }, [doSetSize]);
+
+  React.useEffect(() => {
+    doSetSize();
+  });
+
+  return size;
 }
